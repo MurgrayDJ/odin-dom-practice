2012cc3dfa1d6aa596a539be82567a9fba6cb6da
MurgrayDJ <murgraydj@gmail.com>
2022-08-19 18:32:05 -0400

    Completes question 2 of the exercises
    Creates an h3 element with blue text saying "I'm a blue h3" and appends
    it to the body.

--- script.js
+++ script.js
@@ -15,4 +15,8 @@ paragraph.textContent = "Hey I'm red!";
 paragraph.style['color'] = "red";
 container.appendChild(paragraph);
 
-
+//Question 2
+const heading3 = document.createElement('h3');
+heading3.textContent = "I'm a blue h3!";
+heading3.style.color = 'blue';
+document.body.appendChild(heading3);
