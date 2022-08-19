10e02c9a3858f164fba0d71b7954f8678838d3a4
MurgrayDJ <murgraydj@gmail.com>
2022-08-19 18:30:30 -0400

    Completes question 1 of the exercises
    Creates a paragraph element nested in a container with red text saying
    "Hey I'm red!"

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//Odin project code
 const container = document.querySelector('#container');
 
 const content = document.createElement('div');
@@ -5,3 +7,12 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+//End of Odin project code
+
+//Question 1
+const paragraph = document.createElement('p');
+paragraph.textContent = "Hey I'm red!";
+paragraph.style['color'] = "red";
+container.appendChild(paragraph);
+
+
